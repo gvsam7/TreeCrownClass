3b3703d622381b75e297a5e4de4469ab6c33b9e3
Georgios Voulgaris <gvsam7@yahoo.com>
2025-07-08 17:33:09 +0100

    added utilities, plots

--- Main.py
+++ Main.py
@@ -8,3 +8,75 @@ Description: The aim of this project is to apply Deep Learning techniques, to cl
              tree species from Wytham forest in Oxford.
              Moreover, conducting an investigation regarding Deep Learning Multimodal data fusion.
 """
+
+# Imports
+import torch
+from torch import optim
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Subset
+from sklearn.model_selection import train_test_split
+from torchvision.datasets import ImageFolder
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.metrics import accuracy_score
+import wandb
+import sys
+import os
+from utilities.Save import save_checkpoint, load_checkpoint
+from utilities.Data import DataRetrieve
+from utilities.Config import train_transforms, val_transforms, test_transforms
+from utilities.Networks import networks
+from utilities.Hyperparameters import arguments
+from plots.ModelExam import parameters, get_predictions, plot_confusion_matrix, plot_most_incorrect, get_representations, get_pca, plot_representations, get_tsne
+
+
+def step(data, targets, model, optimizer, criterion, train):
+    with torch.set_grad_enabled(train):
+        outputs = model(data)
+        acc = outputs.argmax(dim=1).eq(targets).sum().item()
+        loss = criterion(outputs, targets)
+
+    if train:
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    return acc, loss
+
+
+@torch.no_grad()
+def get_all_preds(model, loader, device):
+    all_preds = []
+    for x, _ in loader:
+        x = x.to(device)
+        preds = model(x)
+        all_preds.append(preds)
+    all_preds = torch.cat(all_preds, dim=0).cpu()
+    return all_preds
+
+
+def main():
+    args = arguments()
+    wandb.init(project="TreeCrownClass", config=args)
+
+    # Set device
+    if torch.cuda.is_available():
+        device = torch.device(f"cuda:{args.device}")  # Can continue going on here, like cuda:1 cuda:2....etc.
+        print("Running on the GPU")
+    else:
+        device = torch.device("cpu")
+        print("Running on the CPU")
+
+    # Load Data
+    if args.dataset == 'TreeCrown512':
+        dataset = ImageFolder("TreeCrown512")
+    elif args.dataset == 'TreeCrown256':
+        dataset = ImageFolder("TreeCrown256")
+    elif args.dataset == 'TreeCrown128':
+        dataset = ImageFolder("TreeCrown128")
+    else:
+        dataset = ImageFolder("TreeCrown64")
+    print(f"Dataset is {args.dataset}")
---
+++ utilities/Hyperparameters.py
@@ -0,0 +1,42 @@
+import argparse
+
+
+# Hyperparameters
+def arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--epochs", type=int, default=102)
+    parser.add_argument("--in-channels", type=int, default=3)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--batch-size", type=int, default=100)
+    parser.add_argument("--random-state", type=int, default=21)
+    parser.add_argument("--height", type=int, default=256)
+    parser.add_argument("--width", type=int, default=256)
+    parser.add_argument("--save-model", default=False)
+    parser.add_argument("--load-model", default=False)
+    parser.add_argument("--augmentation", default="cutout", help="cutout, cutmix")
+    parser.add_argument("--Augmentation", default="none", help="none, position, cutout")
+    parser.add_argument("--pretrained", default=True)
+    parser.add_argument("--requires-grad", default=False)
+    parser.add_argument("--global-pooling", default=None)
+    parser.add_argument("--dataset", default="mixed", help="mixed, bw, edge, sobel, wet, dry, bw_wet, edge_wet, "
+                                                           "sobel_wet, bw_dry, edge_dry, sobel_dry, resisc45, eurosat,"
+                                                           "realproduct, productreal, realart, artreal, realclipart,"
+                                                           "clipartreal, productclipart, clipartproduct, productart,"
+                                                           "artproduct, artclipart, clipartart")
+    parser.add_argument("--architecture", default="cnn4", help="cnn2=CNN2, cnn4=CNN4, cnn5=CNN5, vgg11=VGG11, vgg13=VGG13, oldcnn3=OldCNN3"
+                                                                    "camcnn2=CAMCNN2, oldcnn4=OldCNN4, oldcnn5=OldCNN5, vgg16=VGG16,"
+                                                                    "tlvgg16=pretrain VGG16, vgg19=VGG19, alexnet=AlexNet"
+                                                                    "resnet18=ResNet18, tlresnet18=pretrain ResNet18,"
+                                                                    "dilresnet18=DilResNet18, gabresnet18=GabResNet18,"
+                                                                    "dilgabresnet18=DilGabResNet18,"
+                                                                    "mpdilgabresnet18=MPDilFGabResNet18"
+                                                                    "resnet50=ResNet50, resnet101=ResNet101,"
+                                                                    "resnet152=ResNet=152, tlalexnet=pretrain AlexNet,"
+                                                                    "sppcnn=SSPCNN, BagnetCustom32, BagnetCustom96Thin,"
+                                                                    "gaborcnn=GaborCNN, gaborcnnmaxp=GaborCNNMaxP,"
+                                                                    "cnn5mixp=CNN5MixP, cnn5maxp=CNN5MaxP,"
+                                                                    "gaborcnnmixp=GaborCNNMixP, dilgabcnn = DilGaborCNN,"
+                                                               "acdilgabcnn = ACDilGaborCNN")
+
+    return parser.parse_args()
