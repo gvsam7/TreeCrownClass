e636a08f314d30e5cb10b4b7b29d27822a11d21d
Georgios Voulgaris <gvsam7@yahoo.com>
2025-07-10 15:00:49 +0100

    debug expansion

--- models/Block.py
+++ models/Block.py
@@ -2,9 +2,10 @@ import torch.nn as nn
 
 
 class Block(nn.Module):
+    expansion = 4  # No of channels after a block is 4*what is was when it entered
+
     def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
         super(Block, self).__init__()
-        self.expansion = 4  # No of channels after a block is 4*what is was when it entered
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
