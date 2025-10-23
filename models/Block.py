e8be47570216a901ea108936f9153225c4b6f63b
Georgios Voulgaris <gvsam7@yahoo.com>
2025-10-23 15:04:11 +0100

    debug dilconv

--- models/Block.py
+++ models/Block.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 
@@ -75,29 +74,13 @@ class DACBlock(nn.Module):
         self.conv6 = nn.Conv2d(in_planes, out_planes, kernel_size=3, padding='same', stride=1, dilation=6)
         self.conv9 = nn.Conv2d(in_planes, out_planes, kernel_size=3, padding='same', stride=1, dilation=9)
         self.bn = nn.BatchNorm2d(out_planes)
-        # self.relu = nn.ReLU(True)
-        self.relu = nn.ReLU(inplace=False)
-
-        # if channel dims can differ later, consider projecting residual
-        if in_planes != out_planes:
-            self.res_proj = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False)
-        else:
-            self.res_proj = None
+        self.relu = nn.ReLU(True)
+        # self.relu = nn.ReLU(inplace=False)
 
     def forward(self, x):
         x1 = self.conv1(x)
         x3 = self.conv3(x)
         x6 = self.conv6(x)
         x9 = self.conv9(x)
-        # x_t = x1 + (x3 + x6 + x9)/3
-        # return self.relu(self.bn(x_t))
-
-        # average using stack to avoid aliasing/views
-        out = torch.stack([x1, x3, x6, x9], dim=0).mean(dim=0)
-        out = self.bn(out)
-
-        # project residual if needed, do out-of-place add
-        residual = x if self.res_proj is None else self.res_proj(x)
-        out = out + residual
-
-        return self.relu(out)
+        x_t = x1 + (x3 + x6 + x9)/3
+        return self.relu(self.bn(x_t))
